a819ce2bb9e7acbcb77135e2aefad5d7e9760598
hmtts <hmttsm@gmail.com>
2022-01-09 00:45:52 +0200

    4.2 realisation of the LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,12 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,8 +16,8 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+def test_should_be_login_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_page()
