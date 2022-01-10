906fa7860ff25a01a61fd28ae7e120acb8619122
hmtts <hmttsm@gmail.com>
2022-01-10 21:23:39 +0200

    4.3.4 bug with promo offer founded > marked as xfail

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,17 @@
+# running param: -v -rx --tb=line --language=en
+import pytest
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+promo_offers = ["offer0", "offer1", "offer2", "offer3", "offer4",
+                "offer5", "offer6",
+                pytest.param("offer7", marks=pytest.mark.xfail(reason="Won't finish")),
+                "offer8", "offer9"]
+
+
+@pytest.mark.parametrize('promo_offer', promo_offers)
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_basket_button()
